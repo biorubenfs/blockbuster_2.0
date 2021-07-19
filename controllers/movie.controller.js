f288aeeea186f04785c051cfbcc5a34731900d74
biorubenfs <biorubenfs@hotmail.com>
2021-07-19 18:15:28 +0200

    find by title and by id added

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -1,5 +1,15 @@
 import Movie from '../models/movie.model.js';
 
+// A simple method to format movie without some attributes
+const formatMovie = (movie) => {
+
+    delete movie.__v;
+    delete movie.created_at;
+    delete movie.updated_at;
+
+    return movie;
+}
+
 export const movieController = {
 
     listMovies: async (req, res) => {
@@ -9,5 +19,40 @@ export const movieController = {
         } catch (error) {
             req.status(400).json({ message: error.message });
         }
+    },
+
+    findMovieById: async (req, res) => {
+
+        try {
+            const result = await Movie.findById(req.params.id).lean();
+
+            if (!result) {
+                res.status(404).send({ message: "movie not found" });
+            }
+            res.json(formatMovie(result));
+
+        } catch (error) {
+            res.json({ message: error.message });
+        }
+    },
+
+    findMovieByTitle: async (req, res) => {
+
+        try {
+            const queryTerm = req.params.title;
+
+            // const query = await Movie.find({ title: /${queryTerm}/i });
+
+            const query = await Movie.find({
+                title: {
+                    $regex: queryTerm,
+                    $options: "i"
+                }
+            });
+
+            res.status(200).json(query);
+        } catch (error) {
+            req.json({ message: error.message });
+        };
     }
 };
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -3,6 +3,8 @@ import { movieController } from '../controllers/movie.controller.js';
 
 const movieRoutes = Router();
 
-movieRoutes.get('/', movieController.listMovies);
+// movieRoutes.get('/', movieController.listMovies);
+movieRoutes.get('/:id', movieController.findMovieById);
+movieRoutes.get('/title/:title', movieController.findMovieByTitle);
 
 export default movieRoutes;
